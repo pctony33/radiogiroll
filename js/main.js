92798860c3cd942a1c60b250af971d1be4ddd256
Antoine Ardilouze <a.ardilouze@alienor.net>
2013-03-03 18:57:10 +0100

    [1.0.3] Nettoyage pour ouverture du code

--- js/main.js
+++ js/main.js
@@ -1,43 +1,44 @@
+/* Init */
+
 play = 0;
+flux = "http://oxyradio.net:8000/";
+
+/* Gestion du player */
 
 $(document).ready(function(){
-
-$('#onair').hide();
-
-$("#listenRadioButton").click("click tap", function(){
-    if(play == 0) {
-        play = -1;
-        $.mobile.showPageLoadingMsg();
-        $('#'+this.id).hide();
-        $('#choicequality').hide();
-        radio = new Media("http://oxyradio.net:8000/"+$('input[name=quality]:checked').val()+".mp3", onSuccess, onError);
-        radio.play();
-
-        var mediaTimer = setInterval(function() {
-            radio.getCurrentPosition(
-                function(position) {
-                    if (position > 1) {
-                        play = 1;
-                        $.mobile.hidePageLoadingMsg();
-                        $('#listenRadioButton .ui-btn-text').text('Stop !');
-                        $('#listenRadioButton').show();
-                        $('#onair').show();
-                        clearInterval(mediaTimer);
+    $('#onair').hide();
+    $("#listenRadioButton").click("click tap", function(){
+        if(play == 0) {
+            play = -1;
+            $.mobile.showPageLoadingMsg();
+            $('#'+this.id).hide();
+            $('#choicequality').hide();
+            radio = new Media(flux+$('input[name=quality]:checked').val()+".mp3", onSuccess, onError);
+            radio.play();
+            var mediaTimer = setInterval(function() {
+                radio.getCurrentPosition(
+                    function(position) {
+                        if (position > 1) {
+                            play = 1;
+                            $.mobile.hidePageLoadingMsg();
+                            $('#listenRadioButton .ui-btn-text').text('Stop !');
+                            $('#listenRadioButton').show();
+                            $('#onair').show();
+                            clearInterval(mediaTimer);
+                        }
+                    },
+                    function(e) {
                     }
-                },
-                function(e) {
-                }
-            );
-        }, 1000);
-
-    } else if(play == 1) {
-        play = 0;
-        $('#'+this.id+' .ui-btn-text').text('Ecouter la radio !');
-        $(this).removeClass("ui-btn-active");
-        $('#choicequality').show();
-        $('#onair').hide();
-        radio.stop();
-    }
+                );
+            }, 1000);
+        } else if(play == 1) {
+            play = 0;
+            $('#'+this.id+' .ui-btn-text').text('Ecouter la radio !');
+            $(this).removeClass("ui-btn-active");
+            $('#choicequality').show();
+            $('#onair').hide();
+            radio.stop();
+        }
     });
 
 });
@@ -48,9 +49,13 @@ function onSuccess() {
 function onError(error) {
 }
 
+/* Force l'ouverture du naviguateur hors application */
+
 function loadURL(url){ 
     navigator.app.loadUrl(url, { openExternal:true }); 
-} 
+}
+
+/* Annule le history.back(); sur le bouton Back */
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
